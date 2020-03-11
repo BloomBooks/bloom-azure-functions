f1cf4b20586f46907a9ab86e1019a2dbc92b651a
Stephen McConnel <stephen_mcconnel@sil.org>
2020-03-11 16:53:04 -0600

    Fill in more fields in OPDS entries (BL-8089)
    UNKNOWN is a placeholder value to alert us to either data is isn't in
    the books table at all or data that is not filled in for the given book.
    This is just a temporary device: a real catalog would probably just omit
    the field.  I think id, title, and updated are the only required fields.
    
    The show field in the books table should be used to filter whether the
    various links are actually provided.  This has not yet been implemented.

--- opds/index.ts
+++ opds/index.ts
@@ -1,6 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import axios from "axios";
 
+// See https://specs.opds.io/opds-1.2.html for the OPDS catalog standard.
+// See https://validator.w3.org/feed/docs/atom.html for the basic (default) tags
+// See https://www.dublincore.org/specifications/dublin-core/dcmi-terms/ for the Dublin Core
+//     (dcterms) tags
+
+
 const opds: AzureFunction = async function(
   context: Context,
   req: HttpRequest
@@ -15,15 +21,16 @@ export default opds;
 
 async function getCatalog(): Promise<string> {
   const header = `<?xml version="1.0" encoding="UTF-8"?>
-      <feed xmlns="http://www.w3.org/2005/Atom" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dc="http://purl.org/dc/terms/" xmlns:opds="http://opds-spec.org/2010/catalog">
-      
-      <id>https://bloomlibrary.org</id>
-      <title>BloomLibrary Books</title>
-      <updated>${new Date().toISOString()}</updated>`;
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:opds="http://opds-spec.org/2010/catalog">
+  <id>https://bloomlibrary.org</id>
+  <title>BloomLibrary Books</title>
+  <updated>${new Date().toISOString()}</updated>
+`;
 
   try {
     const entries = await getEntries();
-    return header + entries + `</feed>`;
+    return header + entries + `
+</feed>`;
   } catch (err) {
     // todo return a proper error response with the right code and such
     return err;
@@ -34,10 +41,96 @@ async function getEntries(): Promise<any> {
   return new Promise<string>((resolve, reject) => {
     getBooks().then(books =>
       resolve(
-        books.map(b => `<entry><title>${b.title}</title></entry>`).join("\r\n")
+        books.map(b => `  <entry>${getEntryFromBook(b)}  </entry>
+`).join("")
       )
     );
   });
+}
+
+function getEntryFromBook(book:any) : string {
+  let entry : string;
+  entry = `
+    <id>${book.bookInstanceId}</id>
+    <title>${book.title}</title>
+`;
+  if (book.authors && book.authors.length > 0) {
+    book.authors.map(author => {
+      entry = entry + `    <author><name>${author}</name></author>
+`;
+    });
+  } else {
+    entry = entry + `    <author><name>UNKNOWN</name></author>
+`;
+  }
+  entry = entry + `    <contributor type="Illustrator"><name>UNKNOWN</name></contributor>
+`;
+  entry = entry + `    <published>${book.createdAt}</published>
+    <updated>${book.updatedAt}</updated>
+`;
+  if (book.publisher) {
+    entry = entry + `    <dcterms:publisher>${book.publisher}</dcterms:publisher>
+`;
+  } else {
+    entry = entry + `    <dcterms:publisher>UNKNOWN</dcterms:publisher>
+`;
+  }
+  if (book.copyright) {
+    entry = entry + `    <rights>${book.copyright}</rights>
+`;
+  } else {
+    entry = entry + `    <rights>UNKNOWN</rights>
+`;
+  }
+  if (book.license) {
+    entry = entry + `    <dcterms:license>${book.license}</dcterms:license>
+`;
+  } else {
+    entry = entry + `    <dcterms:license>UNKNOWN</dcterms:license>
+`;
+  }
+  if (book.langPointers && book.langPointers.length > 0) {
+    // The Dublin Core standard prefers the ISO 639 code for the language, although
+    // StoryWeaver uses language name in their OPDS catalog.
+    book.langPointers.map(lang => {
+      entry = entry + `    <dcterms:language>${lang.isoCode}</dcterms:language>
+`;
+    });
+  }
+  else if (book.languages && book.languages.length > 0) {
+    book.languages.map(lang => {
+      entry = entry + `    <dcterms:language>${lang}</dcterms:language>
+`;
+    });
+  } else {
+    entry = entry + `    <dcterms:language>UNKNOWN</dcterms:language>
+`;
+  }
+  // CHECK: is there a standard book image file we should offer?
+  const baseUrl = book.baseUrl.replace(/%2f/g, "/"); // I don't know why anyone thinks / needs to be url-encoded.
+  const urlWithoutFinalSlash = baseUrl.replace(/\/$/,"");
+  const name = urlWithoutFinalSlash.substring(urlWithoutFinalSlash.lastIndexOf('/')+1);
+  const harvestHead = baseUrl.includes("/BloomLibraryBooks-Sandbox/") ?
+    "https://s3.amazonaws.com/bloomharvest-sandbox/" :
+    "https://s3.amazonaws.com/bloomharvest/";
+  const safeUploader = book.uploader ? MakeUrlSafe(book.uploader.username) : "UNKNOWN";
+  const pdfLink = baseUrl + name + ".pdf";
+  const epubLink = harvestHead + safeUploader + "/" + book.bookInstanceId + "/epub/" + name + ".epub";
+  const bloomdLink = harvestHead + safeUploader + "/" + book.bookInstanceId + "/" + name + ".bloomd";
+  entry = entry + `    <link rel="http://opds-spec.org/acquisition" href="${pdfLink}" type="application/pdf" />
+`;
+  entry = entry + `    <link rel="http://opds-spec.org/acquisition" href="${epubLink}" type="application/epub+zip" />
+`;
+  entry = entry + `    <link rel="http://opds-spec.org/acquisition" href="${bloomdLink}" type="application/bloomd+zip" />
+`;
+  //entry = entry + `<xx:test>${JSON.stringify(b)}</xx:test>`
+  return entry;
+}
+
+function MakeUrlSafe(text: string) : string {
+  // This needs to match whatever Harvester is using.  The first replace is probably enough.
+  var text1 = text.replace("@", "%40");
+  return text1.replace(/ /g, "+");
 }
 
 function getBooks(): Promise<any[]> {
@@ -49,7 +142,7 @@ function getBooks(): Promise<any[]> {
           headers: {
             "X-Parse-Application-Id": "yrXftBF6mbAuVu3fO6LnhCJiHxZPIdE7gl1DUVGR"
           },
-          params: { limit: 10 }
+          params: { limit: 10, include: "uploader,langPointers" }
         }
       )
       .then(result => {
@@ -75,4 +168,3 @@ function getBooks(): Promise<any[]> {
 //       <link rel="http://opds-spec.org/acquisition" href="https://storyweaver.org.in/api/v0/story/pdf/SW-111186" type="application/pdf+zip" />
 //       <link rel="http://opds-spec.org/acquisition" href="https://storyweaver.org.in/api/v0/story/epub/SW-111186" type="application/epub+zip" />
 //       </entry>
-//       `;
