3d3a2d80969a87a58c2021c97958388cdcd4cb22
andrew-polk <andrew_polk@sil.org>
2020-06-26 12:32:59 -0700

    Add ability to process a parse query of books (BL-8130)

--- stats/index.ts
+++ stats/index.ts
@@ -1,16 +1,30 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import BookQuery from "./bookQuery";
 
 const stats: AzureFunction = async function(
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  const book = req.query.book; // || (req.body && req.body.book);
-  const bookInstanceId = req.query["book-instance-id"];
-  const publisher = req.query.publisher;
-  const from = req.query.from || "20000101";
-  const to = req.query.to || "30000101";
+  const book = req.query.book || (req.body && req.body.book);
+  const bookInstanceId =
+    req.query["book-instance-id"] || (req.body && req.body["book-instance-id"]);
 
-  if (book || publisher) {
+  // We aren't actually using these yet.
+  // When we do, we should consider validating them here to guard against sql injection.
+  // And likely we should let the database handle the default so it can be more performant if possible.
+  // const from = req.query.from || (req.body && req.body.from) || "20000101";
+  // const to = req.query.to || (req.body && req.body.to) || "30000101";
+
+  const bookQuery =
+    req.query["book-query"] || (req.body && req.body["book-query"]);
+
+  // We may end up not using this and just using bookQuery instead...
+  const publisher = req.query.publisher || (req.body && req.body.publisher);
+
+  if (bookQuery) {
+    await BookQuery.processStats(context, bookQuery); //, from, to);
+    return;
+  } else if (book || publisher) {
     const { Client } = require("pg");
     const client = new Client();
     await client.connect();
@@ -18,13 +32,13 @@ const stats: AzureFunction = async function(
     let query;
     if (book)
       query = client.query(
-        "SELECT * FROM public.get_book_stats($1, $2, $3, $4)",
-        [book, bookInstanceId, from, to]
+        "SELECT * FROM public.get_book_stats($1, $2)", //, $3, $4)",
+        [book, bookInstanceId] //, from, to]
       );
     else
       query = client.query(
-        "SELECT * FROM public.get_publisher_stats($1, $2, $3)",
-        [publisher, from, to]
+        "SELECT * FROM public.get_publisher_stats($1)", //, $2, $3)",
+        [publisher] //, from, to]
       );
     const queryResult = await query;
     context.res = {
@@ -37,7 +51,8 @@ const stats: AzureFunction = async function(
   } else {
     context.res = {
       status: 400,
-      body: "Please pass a book or publisher in the query string",
+      body:
+        "Please pass a book, publisher, or book query in the query string or body",
     };
   }
 };
