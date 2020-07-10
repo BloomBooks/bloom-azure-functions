e4e2bd13ba8cda933a04e2b07c3406455f85a34f
Jeffrey Su <jeffrey_su@sil.org>
2020-07-10 10:54:11 -0500

    Move Parse query from separate file into main file

--- stats/events.ts
+++ stats/events.ts
@@ -1,7 +1,6 @@
 import { Context } from "@azure/functions";
-import { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { IFilter } from ".";
-import { addParseBooksToTempTableQuery } from "./readingPerDayEventsByParseDBQuery";
 
 const moment = require("moment");
 
@@ -74,6 +73,54 @@ function getDatesFromFilter(filter: IFilter): [string, string] {
   if (!toDate) toDate = "9999-12-31";
 
   return [fromDate, toDate];
+}
+
+async function addParseBooksToTempTableQuery(
+  bookQuery: { url: string; options: AxiosRequestConfig },
+  fromDateValidatedStr: string | undefined,
+  toDateValidatedStr: string | undefined
+): Promise<string | undefined> {
+  // Send query to parse
+  const response = await axios.get(bookQuery.url, bookQuery.options);
+
+  // Make a temp table of the book IDs and book instance IDs in postgres
+  // and
+  // Query against that table in postgres
+  if (response.status === 200 && response.data && response.data.results) {
+    const booksInfo = response.data.results;
+
+    // Return right away if booksInfo.length is 0. No point generating a SQL query
+    if (!booksInfo || booksInfo.length === 0) {
+      console.log("No results returned from Parse");
+      return undefined;
+    }
+
+    const booksInfoFormattedForInsert: string = booksInfo
+      .map(
+        (b: { objectId: string; bookInstanceId: string }) =>
+          `('${b.objectId}','${b.bookInstanceId}')`
+      )
+      .join(",");
+
+    // Crude check for sql injection...
+    if (booksInfoFormattedForInsert.includes(";")) {
+      // ENHANCE: Check that none of the objectIds nor bookInstanceIds have ' in them.
+      console.log(
+        "booksInfoFormattedForInsert = " + booksInfoFormattedForInsert
+      );
+      throw new Error("Unexpected book info caused stats lookup to fail");
+    }
+
+    // true to query based on book IDs in the temp table
+    let sqlFunctionParameters = "true";
+    if (fromDateValidatedStr && toDateValidatedStr) {
+      sqlFunctionParameters += `, '${fromDateValidatedStr}', '${toDateValidatedStr}'`;
+    }
+
+    return `CREATE TEMP TABLE temp_book_ids(book_id,book_instance_id) AS VALUES ${booksInfoFormattedForInsert}; `;
+  } else {
+    throw new Error("Invalid book query");
+  }
 }
 
 async function getCombinedParseAndOrSqlFunction(
