f33adf86e8f9273f1188c17223b4821f60df1793
andrew-polk <andrew_polk@sil.org>
2020-07-29 15:45:21 -0700

    Add daily timer to refresh materialized views

---
+++ dailyTimer/index.ts
@@ -0,0 +1,45 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const timerTrigger: AzureFunction = async function(
+  context: Context,
+  dailyTimer: any
+): Promise<void> {
+  context.log("dailyTimer trigger function started", new Date().toISOString());
+
+  if (dailyTimer.isPastDue) {
+    context.log("dailyTimer trigger function is running late");
+  }
+
+  try {
+    await refreshMaterializedViews();
+    context.log("refreshMaterializedViews() succeeded");
+  } catch (e) {
+    context.log("refreshMaterializedViews() failed", e);
+  }
+
+  context.log("dailyTimer trigger function finished", new Date().toISOString());
+  context.done();
+};
+
+async function refreshMaterializedViews() {
+  // These environment variables are used by pg (node-postgres).
+  // We leave them normally set to the values for the `stats` user which is safer
+  // (and used by the stats function).
+  // But here we need elevated privileges, so we temporarily copy them over from separate env variables
+  // for the silpgadmin user.
+  process.env.PGUSER = process.env.PGUSER_silpgadmin;
+  process.env.PGHOST = process.env.PGHOST_silpgadmin;
+  process.env.PGPASSWORD = process.env.PGPASSWORD_silpgadmin;
+  process.env.PGDATABASE = process.env.PGDATABASE_silpgadmin;
+  process.env.PGPORT = process.env.PGPORT_silpgadmin;
+
+  const { Client } = require("pg");
+  const client = new Client();
+  await client.connect();
+
+  await client.query("select common.refresh_materialized_views()");
+
+  await client.end();
+}
+
+export default timerTrigger;
