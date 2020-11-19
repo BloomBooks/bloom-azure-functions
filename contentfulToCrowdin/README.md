a3c38d4407b0efcf169fb62dd325e46d3418fac8
John Hatton <hattonjohn@gmail.com>
2020-11-19 08:03:07 -0700

    Pull directly from contentful

--- contentfulToCrowdin/README.md
+++ contentfulToCrowdin/README.md
@@ -2,23 +2,11 @@ What this code does (or may do eventually)
 
 # 1 Get Contentful Export
 
-Future:
-Pull down the latest Bloom Library strings in Contentful.com that we want to be translated using Crowdin.com.
-
-For now:
-
-1) Install the contentful cli: 
-
-2) Get the export file `contentful space export --config contentful-config.json`. This will create the file "contentful-export.json" in this directory (it is git-ignored).
+Pulls down the latest Bloom Library strings in Contentful.com that we want to be translated using Crowdin.com.
 
 
 # 2 Produce a json file in the format that Contentful calls "Chrome JSON"
 
-For now:
-
-1) Have typescript installed globally, e.g. `npm add -g typescript`
-2) On the command line from the root of this folder, `ts-node index.ts`
-3) It will place a file named "bloom-library-contentful.json" in this directory (it is git-ignored).
 
 # 3 Push that file to Crowdin
 
@@ -30,3 +18,10 @@ For now: Manually upload it to Crowdin
 # Note: Not yet a Azure Function
 
 This is not yet an actual azure function. It lives in this repo because it will ideally become a function that is auto-run once a day or whatever.
+
+For now:
+
+1) Have typescript installed globally, e.g. `npm add -g typescript`
+2) On the command line from the root of this folder, `ts-node index.ts`
+3) It will place a file named "bloom-library-contentful.json" in this directory (it is git-ignored).
+
--- contentfulToCrowdin/index.ts
+++ contentfulToCrowdin/index.ts
@@ -2,36 +2,48 @@
 each save (like watch) is to install `npm add -g ts-node-dev` and then
 `ts-node-dev --respawn index.ts`. */
 
-import * as fs from "fs";
-console.log("Starting...");
-const input = JSON.parse(fs.readFileSync("contentful-export.json").toString());
+import * as contentful from "contentful";
 
-const output = {};
-// some kinds of collection labels aren't worth presenting to translators. E.g. "SIL LEAD"
-const kindBlackList = ["Organization", "Project", "Language", "Publisher"];
-input.entries
-  // maybe uncomment this when working on this function to speed things up
-  //.slice(0, 30)
-  .filter(
-    (e) =>
-      !e.fields.kind || kindBlackList.indexOf(e.fields.kind["en-US"]) === -1
-  )
+console.log("Querying...");
 
-  .forEach((e) => {
-    const kind = e.fields.kind ? e.fields.kind["en-US"] : "";
-    output[e.fields.urlKey["en-US"]] = {
-      message: e.fields.label["en-US"],
-      // This uilang=en-US parameter isn't implemented in blorg yet, but it could be in the future and could be useful for testing
-      // thing. Meanwhile it does not harm.
-      description: `label for the ${kind} collection. See "https://alpha.bloomlibrary.org/${e.fields.urlKey["en-US"]}?uilang=en-US"`,
-    };
-  });
+const client = contentful.createClient({
+  space: "72i7e2mqidxz",
+  accessToken: "XPudkny5JX74w0dxrwqS_WY3GUBA5xO_AzFR7fwO2aE",
+});
 
-// uncomment this when working on this function; use with ts-node-dev (see readme)
-//console.log(output);
+client
+  .getEntries({ content_type: "collection" })
+  .then((response) => transform(response.items))
+  .catch((err) => console.log(err));
 
-fs.writeFileSync(
-  "./bloom-library-contentful.json",
-  JSON.stringify(output, null, 4)
-);
-console.log("Done.");
+function transform(entries: any[]) {
+  console.log(`transforming ${entries.length} entries...`);
+  const output = {};
+  // some kinds of collection labels aren't worth presenting to translators. E.g. "SIL LEAD"
+  const kindBlackList = ["Organization", "Project", "Language", "Publisher"];
+  entries
+    // maybe uncomment this when working on this function to speed things up
+    .slice(0, 30)
+    .filter(
+      (e) => !e.fields.kind || kindBlackList.indexOf(e.fields.kind) === -1
+    )
+
+    .forEach((e) => {
+      const kind = e.fields.kind ? e.fields.kind : "";
+      output[e.sys.contentType.sys.id + "." + e.fields.urlKey] = {
+        message: e.fields.label,
+        // This uilang=en-US parameter isn't implemented in blorg yet, but it could be in the future and could be useful for testing
+        // thing. Meanwhile it does not harm.
+        description: `label for the BloomLibrary ${kind} collection. See "https://alpha.bloomlibrary.org/${e.fields.urlKey}?uilang=en-US"`,
+      };
+    });
+
+  // // uncomment this when working on this function; use with ts-node-dev (see readme)
+  console.log(output);
+
+  // fs.writeFileSync(
+  //   "./bloom-library-contentful.json",
+  //   JSON.stringify(output, null, 4)
+  // );
+  console.log("Done.");
+}
