102523c931550f58a7585936e0be9aafaf1cd12f
Andrew Polk <andrew_polk@sil.org>
2023-11-22 07:35:15 -0700

    Refactor (BL-12726)

--- book/get.ts
+++ book/get.ts
@@ -6,10 +6,12 @@ export async function handleGet(
   parseServer: BloomParseServer,
   context: Context,
   req: HttpRequest,
-  action: string,
   userInfo: User
 ) {
-  switch (action) {
+  switch (req.params.action) {
+    case "get-book-count-by-language":
+      await getBookCountByLanguage(context, req, parseServer);
+      return;
     case "can-modify-book":
       await canModifyBook(parseServer, context, req, userInfo);
       return;
@@ -20,6 +22,24 @@ export async function handleGet(
       };
       return;
   }
+}
+
+// Get the number of books on bloomlibrary.org that are in the given language. Query should get all books where the isoCode matches the given languageCode
+// and 'rebrand' is not true and 'inCirculation' is not false and 'draft' is not true.
+// In the future, we may need to parameterize those filters, but for now, it fits our current use case (Bloom editor's count of books uploaded).
+async function getBookCountByLanguage(
+  context: Context,
+  req: HttpRequest,
+  parseServer: BloomParseServer
+) {
+  const queryParams = req.query;
+  const languageTag = queryParams["language-tag"];
+  const count = await parseServer.getBookCountByLanguage(languageTag);
+
+  context.res = {
+    status: 200,
+    body: count,
+  };
 }
 
 async function canModifyBook(
--- book/index.ts
+++ book/index.ts
@@ -1,10 +1,9 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import BloomParseServer, { User } from "../common/BloomParseServer";
 import { getEnvironment } from "../common/utils";
-import { handleUploadStart } from "./uploadStart";
-import { handleUploadFinish } from "./uploadFinish";
 import { handleDelete } from "./delete";
 import { handleGet } from "./get";
+import { handlePost } from "./post";
 
 const book: AzureFunction = async function (
   context: Context,
@@ -13,64 +12,34 @@ const book: AzureFunction = async function (
   const env = getEnvironment(req);
   const parseServer = new BloomParseServer(env);
 
+  const actionsWhichDoNotRequireAuthentication = [
+    "get-books",
+    "get-book-count-by-language",
+  ];
+
   let userInfo = null;
-
-  if (req.method === "GET") {
-    await handleGet(parseServer, context, req, req.params.action, userInfo);
-    return;
-  }
-  if (req.method === "DELETE") {
-    await handleDelete(parseServer, context, req, userInfo);
-    return;
-  }
-
-  if (req.method === "POST") {
-    const userInfo = await getUserFromSession(parseServer, req);
-    // for actions for which we need to validate the authentication token
-    if (!userInfo && req.params.action in ["upload-start", "upload-finish"]) {
+  if (!(req.params.action in actionsWhichDoNotRequireAuthentication)) {
+    userInfo = await getUserFromSession(parseServer, req);
+    if (!userInfo) {
       context.res = {
         status: 400,
         body: "Unable to validate user. Did you include a valid Authentication-Token header?",
       };
       return;
     }
+  }
 
-    switch (req.params.action) {
-      case "upload-start":
-        await handleUploadStart(context, req, parseServer, userInfo, env);
-        return;
-      case "upload-finish":
-        await handleUploadFinish(context, req, parseServer, userInfo, env);
-        return;
-      case "get-books":
-        if (req.body.bookInstanceIds !== undefined) {
-          await getBooksWithIds(context, req.body.bookInstanceIds, parseServer);
-        } else {
-          context.res = {
-            status: 400,
-            body: "Please provide bookInstanceIds in the body to get-books",
-          };
-        }
-        return;
-      default:
-        context.res = {
-          status: 400,
-          body: "Invalid action type",
-        };
-        return;
-    }
-  } else {
-    switch (req.params.action) {
-      case "get-book-count-by-language":
-        await getBookCountByLanguage(context, req, parseServer);
-        return;
-      default:
-        context.res = {
-          status: 400,
-          body: "Invalid action type",
-        };
-        return;
-    }
+  if (req.method === "GET") {
+    await handleGet(parseServer, context, req, userInfo);
+    return;
+  }
+  if (req.method === "POST") {
+    await handlePost(parseServer, context, req, userInfo, env);
+    return;
+  }
+  if (req.method === "DELETE") {
+    await handleDelete(parseServer, context, req, userInfo);
+    return;
   }
 };
 
@@ -82,37 +51,6 @@ async function getUserFromSession(
   // Note that req.headers' keys are all lower case.
   const authenticationToken = req.headers["authentication-token"];
   return await parseServer.getLoggedInUserInfo(authenticationToken);
-}
-
-// Get the number of books on bloomlibrary.org that are in the given language. Query should get all books where the isoCode matches the given languageCode
-// and 'rebrand' is not true and 'inCirculation' is not false and 'draft' is not true.
-// In the future, we may need to parameterize those filters, but for now, it fits our current use case (Bloom editor's count of books uploaded).
-async function getBookCountByLanguage(
-  context: Context,
-  req: HttpRequest,
-  parseServer: BloomParseServer
-) {
-  const queryParams = req.query;
-  const languageTag = queryParams["language-tag"];
-  const count = await parseServer.getBookCountByLanguage(languageTag);
-
-  context.res = {
-    status: 200,
-    body: count,
-  };
-}
-
-async function getBooksWithIds(
-  context: Context,
-  bookInstanceIds: string[],
-  parseServer: BloomParseServer
-) {
-  const bookRecords = await parseServer.getBooksWithIds(bookInstanceIds);
-
-  context.res = {
-    status: 200,
-    body: { bookRecords },
-  };
 }
 
 export default book;
